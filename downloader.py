8a268c51fe143683ca04c415b5a95d2450d2f0d8
jekoo <pjkwprn@gmail.com>
2021-01-08 19:37:09 +0900

    feat : 여러개 입력 변환

--- downloader.py
+++ downloader.py
@@ -4,7 +4,9 @@ import video_down
 
 def start(cursor):
     if cursor == "1":
-        mp3down.make_mp3()
+        print("한줄로 입력")
+        urls = list(input().split(","))
+        mp3down.make_mp3(urls)
 
     if cursor == "2":
         video_down.make_video()
@@ -16,3 +18,7 @@ start(cursor)
 
 # https://www.youtube.com/watch?v=5EC3_-mhHp0
 # https://www.youtube.com/watch?v=lOrU0MH0bMk
+
+# 입력형식:
+# https://www.youtube.com/watch?v=00VK7fE8pts,https://www.youtube.com/watch?v=qtf4-D6V_2o
+# https://www.youtube.com/watch?v=qKkmEscb6rg,https://www.youtube.com/watch?v=jwa3qtEgHME,https://www.youtube.com/watch?v=0VBdoHJe1gc
--- mp3down.py
+++ mp3down.py
@@ -1,14 +1,16 @@
 import os
 
 
-def make_mp3():
-    try:
-        print("유튜브 주소 검색(mp3)")
-        url = input()
-        myurl = '"' + url + '"'
-        command = "youtube-dl --extract-audio --audio-format mp3 %s" % myurl
-        os.chdir('/Users/zigje9/Desktop/mp3_down/mp3/')
-        os.system(command)
-    except:
-        print("에러 발생 주소를 다시확인하세요")
+def make_mp3(urls):
+    for url in urls:
+        try:
+            myurl = '"' + url + '"'
+            command = "youtube-dl --extract-audio --audio-format mp3 %s" % myurl
+            os.chdir('/Users/zigje9/Desktop/mp3_down/mp3/')
+            os.system(command)
+        except:
+            print("에러 발생 주소를 다시확인하세요")
 
+#https://www.youtube.com/watch?v=0Okrc8l1yYI
+#https://www.youtube.com/watch?v=d3JPp_5kgO4
+#https://www.youtube.com/watch?v=uFAKthN6BfQ
